728fe6c2af030f2a9288eadce1bdf0cf0745957b
Giovanni Cristofoletti <giovanni.cristofoletti@gmail.com>
2013-10-09 21:38:27 +0200

    MINOR remove tests

--- code/SortableUploadField.php
+++ code/SortableUploadField.php
@@ -12,9 +12,6 @@
 
         public function __construct($name, $title = null, SS_List $items = null) {
             parent::__construct($name, $title = null, $items = null);
-
-            $this->setConfig("test","test");
-            $this->setAttribute("test","Tes");
         }
 
         public function Field($properties = array()) {
